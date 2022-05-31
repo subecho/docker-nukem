67870afa2131b427b66e6b4da6c3e6df27bbf738
Dustin Schoenbrun <dschoenbrun@juniper.net>
2022-05-31 15:29:20 -0400

    Use Moby Client to list containers

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/moby/moby/client"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
+}
